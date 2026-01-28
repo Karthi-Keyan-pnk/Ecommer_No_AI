1107ab8aa64bfa22760f2dc9bc670127b0a53a96
Karthikeyan <karthikeyan.pnk30@gmail.com>
2026-01-28 21:40:06 +0530

    Crud Without JPA Repo ,Using the User Created data

--- src/main/java/com/example/CRUD/Module/Product.java
+++ src/main/java/com/example/CRUD/Module/Product.java
@@ -4,16 +4,23 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int Id;
     private String Name;
     private double Price;
+
+    public Product(String str, double v) {
+        this.Name = str;
+        this.Price = v;
+    }
 }
--- src/main/java/com/example/CRUD/Service/ProductService.java
+++ src/main/java/com/example/CRUD/Service/ProductService.java
@@ -4,22 +4,29 @@ import com.example.CRUD.Module.Product;
 import com.example.CRUD.Repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    ProductRepository productRepository;
-
-    public ProductService(ProductRepository productRepository){
-        this.productRepository = productRepository;
-    }
+    List<Product> products = new ArrayList<>(Arrays.asList(
+            new Product(1,"Kumar", 2.00),
+            new Product(2,"Gopi", 1.00),
+            new Product(3,"hari", 1.20)));
 
     public List<Product> getProducts() {
-        return productRepository.findAll();
+        return products;
     }
 
     public Product getProductById(int id) {
-        return productRepository.findById(id).get();
+       return products.stream()
+        .filter(p1->p1.getId() == id)
+               .findFirst()
+                .orElse(new Product(100,"NoItem",0));
+        // OR
+        // return products.get(id);
     }
+
 }
