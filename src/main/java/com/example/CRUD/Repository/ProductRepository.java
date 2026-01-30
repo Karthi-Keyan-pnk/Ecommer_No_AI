a8511d386a8d9ec986c0952fbbaaf698338af298
Karthikeyan <karthikeyan.pnk30@gmail.com>
2026-01-30 18:49:34 +0530

    Directory changes

--- src/main/java/com/example/CRUD/Repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.CRUD.Repository;
-
-import com.example.CRUD.Module.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Integer> {
-}
